c404a77992cbd5f164c4481a3d193e7ed60f480e
J0na555 <ethiopediaa@gmail.com>
2025-08-27 23:07:29 +0300

    anime: added top anime list and anime search features

---
+++ animes/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('top_anime/', views.top_anime, name= 'top-anime'),
+    path('anime_list/', views.anime_list, name= 'anime-list'),
+]
--- animes/views.py
+++ animes/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+def top_anime(request):
+    page = request.GET.get('page', 1)
+    url = f"https://api.jikan.moe/v4/top/anime?page={page}"
+
+
+    response = requests.get(url)
+    json_data = response.json()
+
+
+
+    anime_list = json_data.get('data', [])
+    pagination = json_data.get('pagination', {})
+    pagination['has_previous_page'] = int(page) > 1
+
+    return render(request, 'animes/top_anime.html', {
+        'animes': anime_list,
+        'pagination': pagination,
+        'current_page': int(page)
+    })
+
+def anime_list(request):
+    query = request.GET.get('q', '')
+    page = request.GET.get('page', 1)
+
+    if query:
+        url = f"https://api.jikan.moe/v4/anime?q={query}&page={page}"
+    else:
+        url = f"https://api.jikan.moe/v4/top/anime?page={page}"
+
+    
+    response = requests.get(url)
+    json_data = response.json()
+
+    anime_list = json_data.get('data', [])
+    pagination = json_data.get('pagination', {})
+    pagination['has_previous_page'] = int(page) > 1
+
+
+    return render(request, 'animes/anime_list.html', {
+        'animes': anime_list,
+        'pagination': pagination,        
+        'query': query,
+        'current_page':int(page),
+    })
+
+    
+
+
+
+
+
+
+
+
+
