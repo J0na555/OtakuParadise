7067e827bc499b61ffffe5577f21540f8d7a1d94
J0na555 <ethiopediaa@gmail.com>
2025-08-28 01:30:01 +0300

    anime:added search by genre, year, score

--- animes/views.py
+++ animes/views.py
@@ -24,19 +24,30 @@ def top_anime(request):
 def anime_list(request):
     query = request.GET.get('q', '')
     page = request.GET.get('page', 1)
+    genres = request.GET.getlist('genres')
+    year = request.GET.get('year', '')
+    min_score = request.GET.get('min_score', '')
 
-    if query:
-        url = f"https://api.jikan.moe/v4/anime?q={query}&page={page}"
-    else:
-        url = f"https://api.jikan.moe/v4/top/anime?page={page}"
+    params = {"page": page, "q": query}
 
+    if genres:
+        params["genres"] = ",".join(genres)
+    if year:
+        params["start_date"] = f"{year}-01-01"
+        params["end_date"] = f"{year}-12-31"
+    if min_score:
+        params["min_score"] = min_score
     
-    response = requests.get(url)
+    response = requests.get("https://api.jikan.moe/v4/anime", params=params)
     json_data = response.json()
-
     anime_list = json_data.get('data', [])
     pagination = json_data.get('pagination', {})
     pagination['has_previous_page'] = int(page) > 1
+
+    #fetch all genres 
+    genre_response = requests.get("https://api.jikan.moe/v4/genres/anime")
+    genre_data = genre_response.json()
+    all_genres = genre_data.get('data', [])
 
 
     return render(request, 'animes/anime_list.html', {
@@ -44,6 +55,10 @@ def anime_list(request):
         'pagination': pagination,        
         'query': query,
         'current_page':int(page),
+        'year': year,
+        'min_score': min_score,
+        'selected_genres': genres,
+        'all_genres': all_genres,
     })
 
     
