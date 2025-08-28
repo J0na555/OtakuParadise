235108dcd0745c88d3fc30603266cacc82282481
J0na555 <ethiopediaa@gmail.com>
2025-08-28 16:23:23 +0300

    manga:added searching manga by name, year, rating and genre features

--- manga/views.py
+++ manga/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+
+def manga_list(request):
+    query = request.GET.get('q', '')
+    page = request.GET.get('page', 1)
+    genres = request.GET.getlist('genres')
+    year = request.GET.get('year', '')
+    min_score = request.GET.get('min_score', '')
+
+    params = {"page": page, "q": query}
+
+    if genres:
+        params["genres"] = ",".join(genres)
+    if year:
+        params["start_date"] = f"{year}-01-01"
+        params["end_date"] = f"{year}-12-31"
+    if min_score:
+        params["min_score"] = min_score
+
+    response = requests.get("https://api.jikan.moe/v4/manga", params=params)
+    json_data = response.json()
+    manga_list = json_data.get('data', [])
+    pagination = json_data.get('pagination', {})
+    pagination['has_previous_page'] = int(page) > 1
+
+
+    genre_response = requests.get("https://api.jikan.moe/v4/genres/manga")
+    genre_data = genre_response.json()
+    all_genres = genre_data.get('data', [])
+
+
+    return render(request, 'manga/manga_list.html',{
+        'mangas': manga_list,
+        'pagination': pagination,
+        'query': query,
+        'current_page': int(page),
+        'year': year,
+        'min_score': min_score,
+        'selected_genres':genres,
+        'all_genres': all_genres,
+    } )
--- otakuparadise/urls.py
+++ otakuparadise/urls.py
@@ -4,9 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('animes.urls')),
-    # path('quotes/', include('quotes.urls')),     
-    # path('characters/', include('characters.urls')),
-    # path('manga/', include('manga.urls')),
-    # path('news/', include('news.urls')),
-    # path('waifus/', include('waifus.urls')),
+    path('manga/', include('manga.urls')),
 ]
