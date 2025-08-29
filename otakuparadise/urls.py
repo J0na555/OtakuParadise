067c7138a2e6079dc4f48fabdccbff7132e2932e
J0na555 <ethiopediaa@gmail.com>
2025-08-29 21:06:56 +0300

    quotes:added searching and filtering quotes by character and anime also in random

--- otakuparadise/urls.py
+++ otakuparadise/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('animes.urls')),
     path('manga/', include('manga.urls')),
+    path('quotes/', include('quotes.urls')),
 ]
