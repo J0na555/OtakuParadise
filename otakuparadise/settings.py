cafcb79c12b7ac4ea4a47061dbca68b4f186f0d8
J0na555 <ethiopediaa@gmail.com>
2025-09-09 17:34:35 +0300

    fixed some deployment issues

--- otakuparadise/settings.py
+++ otakuparadise/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,10 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS  = [
-    BASE_DIR/"static"
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",   #  app-level static files
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"  # where collectstatic will put everything
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
