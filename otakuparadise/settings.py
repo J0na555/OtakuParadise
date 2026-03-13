dd932fb3729483bdd1b617603f1bf3d5d4ad3fd7
J0na555 <ethiopediaa@gmail.com>
2026-03-13 13:03:14 +0300

    Update ALLOWED_HOSTS in settings.py to include production domain

--- otakuparadise/settings.py
+++ otakuparadise/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DEBUG", "True").lower() in ("true", "1", "yes")
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get("otakuparadise-6vx7.onrender.com", "localhost,127.0.0.1").split(",")
 
 # Application definition
 
