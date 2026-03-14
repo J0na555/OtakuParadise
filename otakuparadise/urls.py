57f1d4a4ffc440451f33eff0a2f6a1bf8d6cc327
J0na555 <ethiopediaa@gmail.com>
2026-03-14 09:24:08 +0300

    Add health check endpoint to urls.py

--- otakuparadise/urls.py
+++ otakuparadise/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include
 
+
+def health(request):
+    return JsonResponse({"status": "ok"})
+
+
 urlpatterns = [
+    path('health/', health),
     path('admin/', admin.site.urls),
     path('', include('animes.urls')),
     path('manga/', include('manga.urls')),
